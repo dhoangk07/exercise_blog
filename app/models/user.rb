526bb1728503fdfc2ef8713f94368ee4c98bc5a3
HoangDinh <dhoangk07@gmail.com>
2018-05-08 13:59:38 +0700

    Add validated for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates  :email,     :presence   => true,
+            :format                 => { with: VALID_EMAIL_REGEX },
+            :uniqueness             => {:case_sensitive => false}
+
+  validates  :password,  :presence   => true,
+            :confirmation           => true,
+            :length                 => {:within => 6..40}, :on => :create
 end
