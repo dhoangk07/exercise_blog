9a99b2fbc3b2890196f9a2203a16727569dfd31f
HoangDinh <dhoangk07@gmail.com>
2018-05-08 09:39:39 +0700

    Add detail action for newspaper controller

--- app/controllers/newspapers_controller.rb
+++ app/controllers/newspapers_controller.rb
@@ -1,32 +1,48 @@
 class NewspapersController < ApplicationController
-    before_action :set_newspaper ,only: [:edit, :update, :show, :destroy]
-    def index
+  before_action :set_newspaper ,only: [:edit, :update, :show, :destroy]
+  def index
+    @newspapers = Newspaper.all
+  end
+
+  def new
+    @newspaper = Newspaper.new
+  end
+
+  def create
+    @newspaper = Newspaper.create(newspaper_params)
+    if @newspaper.save
+      redirect_to @newspaper
+    else
+      render "new"
     end
-  
-    def new
+  end 
+
+  def edit
+  end
+
+  def update
+    if @newspaper.update(newspaper_params)
+      redirect_to newspapers_path
+    else 
+      render "edit"
     end
-  
-    def create
-    end 
-  
-    def edit
+  end
+
+  def show
+  end
+
+  def destroy
+    if @newspaper.destroy
+      redirect_to newspapers_path
     end
-  
-    def update
-    end
-  
-    def show
-    end
-  
-    def destroy
-    end
-  
-    def set_newspaper
-      @newspaper = Newspaper.find(params[:id])
-    end
-  
-    private
-    def newspaper_params
-      params.require(:newspaper).permit(:title, :text)
-    end
+  end
+
+  def set_newspaper
+    @newspaper = Newspaper.find(params[:id])
+  end
+
+  private
+  def newspaper_params
+    params.require(:newspaper).permit(:title, :content)
+  end
 end
