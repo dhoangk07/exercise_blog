0c641de24380b8f7e5c36b1f695e2010c7b52777
HoangDinh <dhoangk07@gmail.com>
2018-05-11 01:06:57 +0700

    Create comment by rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,13 +26,22 @@
 # end
 
 # Microposts
-users = User.order(:created_at).take(6)
-50.times do
-  title = "New York"
-  content = Faker::Lorem.sentence(100)
-  users.each { |user| user.newspapers.create!(title: title, content: content) }
-end
+# users = User.order(:created_at).take(6)
+# 50.times do
+#   title = "New York"
+#   content = Faker::Lorem.sentence(100)
+#   users.each { |user| user.newspapers.create!(title: title, content: content) }
+# end
 
+#Comments
+
+newspapers = Newspaper.take(50)
+10.times do
+  commenter = "Hanoi"
+  body = Faker::Lorem.sentence(100)
+  newspapers.each { |newspaper| newspaper.comments.create!(commenter: commenter, body: body, user_id: "56")}
+ 
+end
 # # Following relationships
 # users = User.all
 # user  = users.first
