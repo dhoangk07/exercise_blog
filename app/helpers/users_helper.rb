9f78d504bcb5420245aa3c5cafb8759442ec01cb
HoangDinh <dhoangk07@gmail.com>
2018-05-10 17:02:59 +0700

    Add gravatar for app

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,10 @@ module UsersHelper
       current_user.role == "admin"
     end
   end
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.last_name, class: "gravatar")
+  end
 end
