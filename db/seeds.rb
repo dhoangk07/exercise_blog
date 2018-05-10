1c4059e3fced208b9bcd45cca10876630c2bd7df
HoangDinh <dhoangk07@gmail.com>
2018-05-10 10:22:17 +0700

    Create seed for app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User.create!(name:  "Example User",
+#              email: "example@railstutorial.org",
+#              password:              "foobar",
+#              password_confirmation: "foobar",
+#              admin:     true,
+#              activated: true,
+#              activated_at: Time.zone.now)
+
+99.times do |n|
+  first_name  = Faker::Name.first_name
+  last_name  = Faker::Name.last_name
+  email = "example-#{n+1}@gmail.com"
+  password = "1234567"
+  User.create!(first_name:  first_name,
+               last_name: last_name,
+               email: email,
+               password:              1234567,
+               password_confirmation: 1234567)
+end
+
+# # Microposts
+# users = User.order(:created_at).take(6)
+# 50.times do
+#   content = Faker::Lorem.sentence(5)
+#   users.each { |user| user.microposts.create!(content: content) }
+# end
+
+# # Following relationships
+# users = User.all
+# user  = users.first
+# following = users[2..50]
+# followers = users[3..40]
+# following.each { |followed| user.follow(followed) }
+# followers.each { |follower| follower.follow(user) }
