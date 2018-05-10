41c59e51dc65c62ce44cc220b4232324f767b854
HoangDinh <dhoangk07@gmail.com>
2018-05-10 11:22:11 +0700

    Add action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :destroy]
   def index
-    @users = User.all
+    if params[:filter] == "admin" 
+      @users = User.where(role: params[:filter])
+    else
+      @users = User.all
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to users_path
+      flash[:danger] = "#{@user.first_name} #{@user.last_name} has been deleted "
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
