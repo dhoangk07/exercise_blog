ef0649edee5838f4f04daeea4cd1533696f802be
HoangDinh <dhoangk07@gmail.com>
2018-05-11 22:40:26 +0700

    Decorate app

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,21 +38,21 @@
 # end
 
 # Microposts
-# users = User.order(:created_at).take(6)
-# 50.times do
-#   title = "New York"
-#   content = Faker::Lorem.sentence(100)
-#   users.each { |user| user.newspapers.create!(title: title, content: content) }
-# end
+users = User.order(:created_at).take(6)
+50.times do
+  title = "Thailand"
+  content = Faker::Lorem.sentence(100)
+  users.each { |user| user.newspapers.create!(title: title, content: content) }
+end
 
 #Comments
 
-newspapers = Newspaper.take(50)
-10.times do
-  commenter = "Hanoi"
-  body = Faker::Lorem.sentence(100)
-  newspapers.each { |newspaper| newspaper.comments.create!(commenter: commenter, body: body, user_id: "1")}
-end
+# newspapers = Newspaper.take(50)
+# 10.times do
+#   commenter = "Hanoi"
+#   body = Faker::Lorem.sentence(100)
+#   newspapers.each { |newspaper| newspaper.comments.create!(commenter: commenter, body: body, user_id: "5")}
+# end
 
  
 # end
