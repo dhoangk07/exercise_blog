bd599c632666dcedcb0a87b218d8a796dca12f16
HoangDinh <dhoangk07@gmail.com>
2018-05-12 15:16:28 +0700

    Run add_attachment_image

--- db/migrate/20180510084254_add_attachment_image_to_users.rb
+++ db/migrate/20180510084254_add_attachment_image_to_users.rb
@@ -1,11 +1,11 @@
 class AddAttachmentImageToUsers < ActiveRecord::Migration[5.2]
   def self.up
     change_table :users do |t|
-      # add_attachment :users, :image
+      add_attachment :users, :image
     end
   end
 
   def self.down
-    # remove_attachment :users, :image
+    remove_attachment :users, :image
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_11_025630) do
+ActiveRecord::Schema.define(version: 2018_05_10_084254) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -33,21 +36,6 @@ ActiveRecord::Schema.define(version: 2018_05_11_025630) do
     t.string "user_id"
   end
 
-  create_table "taggings", force: :cascade do |t|
-    t.integer "tag_id"
-    t.integer "newspaper_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["newspaper_id"], name: "index_taggings_on_newspaper_id"
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -64,10 +52,6 @@ ActiveRecord::Schema.define(version: 2018_05_11_025630) do
     t.string "first_name"
     t.string "last_name"
     t.string "role", default: "user"
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.integer "image_file_size"
-    t.datetime "image_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
