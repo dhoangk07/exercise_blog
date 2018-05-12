95bd9cce2ca34e12a3a43a194d5f4a7d7a648e97
HoangDinh <dhoangk07@gmail.com>
2018-05-12 12:56:38 +0700

    add update for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :destroy]
+  before_action :set_user, only: [:show, :destroy, :update]
   def index
     if params[:filter] == "admin" 
       @users = User.where(role: params[:filter]).paginate(:page => params[:page], :per_page => 5)
