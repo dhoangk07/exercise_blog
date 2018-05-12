392ad99d541d03b01746e06d871f630e23de41b0
HoangDinh <dhoangk07@gmail.com>
2018-05-12 13:14:40 +0700

    add edit action for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :destroy, :update]
+  before_action :set_user, only: [:show, :destroy, :update, :edit]
   def index
     if params[:filter] == "admin" 
       @users = User.where(role: params[:filter]).paginate(:page => params[:page], :per_page => 5)
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+
   end
 
   def update
