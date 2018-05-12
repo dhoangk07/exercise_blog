3d72727127a168b8e140c59cf70dbe0cbd98d837
HoangDinh <dhoangk07@gmail.com>
2018-05-12 15:30:56 +0700

    Comment migration to avoid confict

--- db/migrate/20180510084254_add_attachment_image_to_users.rb
+++ db/migrate/20180510084254_add_attachment_image_to_users.rb
@@ -1,11 +1,11 @@
 class AddAttachmentImageToUsers < ActiveRecord::Migration[5.2]
-  def self.up
-    change_table :users do |t|
-      add_attachment :users, :image
-    end
-  end
+  # def self.up
+  #   # change_table :users do |t|
+  #   #   add_attachment :users, :image
+  #   end
+  # end
 
-  def self.down
-    remove_attachment :users, :image
-  end
+  # def self.down
+  #   remove_attachment :users, :image
+  # end
 end
