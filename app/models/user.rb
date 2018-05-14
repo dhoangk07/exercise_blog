e3dd5be6c3e5c9cd517fbda0db0484400e585581
HoangDinh <dhoangk07@gmail.com>
2018-05-14 16:54:30 +0700

    Create size small for user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_many :newspapers, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#" }
+  has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#", small: "70x70#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   def self.search(search)
