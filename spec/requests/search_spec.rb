a7c98ad6a55a10a29c2f88eaa97294998e374a75
HoangDinh <dhoangk07@gmail.com>
2018-05-15 22:45:51 +0700

    Add integration test for search form

--- spec/requests/search_spec.rb
+++ spec/requests/search_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "search form", :type => :request do
+RSpec.describe "search form", :type => :request, js: true do
   it "display result after click search form" do
-    visit "http://localhost:3000/newspapers"
+    visit newspapers_path
+    user_1 = User.create!(first_name: "Hoang", last_name: "Dinh", email: "dhoanghvs@gmail.com", password: "1234567")
+    user_2 = User.create!(first_name: "Huy", last_name: "Dinh", email: "dhuy@gmail.com", password: "1234567")
 
-    user_1 = User.create!(first_name: "Hoang", last_name: "Dinh", email: "dhoanghvs@gmail.com", password: "1234567", user_id: "1")
-    user_2 = User.create!(first_name: "Huy", last_name: "Dinh", email: "dhuy@gmail.com", password: "1234567", user_id: "2")
-
-    VietNam = Newspaper.create!(title: "VietNam", content: "Lorem", user_id: "1", role: "user")
-    Laos = Newspaper.create!(title: "Laos", content: "Lorem1", user_id: "2", role: "user")
+    vietnam = Newspaper.create!(title: "VietNam", content: "Lorem", user_id: user_1.id, role: "user")
+    laos = Newspaper.create!(title: "Laos", content: "Lorem1", user_id: user_2.id, role: "user")
     expect(page).to have_button("Go!")
     expect(page).to have_content("Search")
-    # expect(page).to to respond to has_form?('Search', type: 'text')
-    expect(Newspaper.search("VietNam")).to eq([vietnam])
+
+    fill_in "search", with: "VietNam"
+    click_button "Go!"
+    expect(page).to have_content(vietnam.title)
+    expect(page).not_to have_content(laos.title)
   end
 end
