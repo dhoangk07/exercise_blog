c6540de3a4c0260d568697487ff0d0281c6ee660
HoangDinh <dhoangk07@gmail.com>
2018-05-18 09:05:49 +0700

    fix bug

--- app/helpers/newspapers_helper.rb
+++ app/helpers/newspapers_helper.rb
@@ -3,6 +3,12 @@ module NewspapersHelper
     newspaper.user.first_name.capitalize + " " + newspaper.user.last_name.capitalize
   end
 
+  # def current_writer_full_name(newspaper)
+  #   if newspaper.present?
+  #     newspaper.current_user.first_name.capitalize + " " + newspaper.current_user.last_name.capitalize
+  #   end
+  # end
+
   def switch(direction)
     if direction == 'asc'
       'desc'
