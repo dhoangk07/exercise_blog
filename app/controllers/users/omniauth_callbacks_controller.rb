538995d8e797c9aca50de5788b3fa3dd405e58a4
HoangDinh <dhoangk07@gmail.com>
2018-05-20 14:32:11 +0700

    Remove debugger

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    debugger
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
@@ -13,7 +12,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def failure
-    debugger
     redirect_to root_path
   end
 end
