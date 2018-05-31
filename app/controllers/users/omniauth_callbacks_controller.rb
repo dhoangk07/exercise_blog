0ea1ef81b695f892f283d06e9384d4b62438c974
HoangDinh <dhoangk07@gmail.com>
2018-05-31 23:52:05 +0700

    Comment about split first_name and last_name of user

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,9 +3,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      first_name = @user.name.split(" ").first
-      last_name = @user.name.split(" ").drop(1).join(" ")
-      User.update(first_name: first_name, last_name: last_name)
+      # first_name = @user.name.split(" ").first
+      # last_name = @user.name.split(" ").drop(1).join(" ")
+      # User.update(first_name: first_name, last_name: last_name)
       sign_in_and_redirect @user, :event => :authentication
 
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
