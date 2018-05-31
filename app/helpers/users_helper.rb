07d200c69999ebcd3660231cd78aecb9cef6cf38
HoangDinh <dhoangk07@gmail.com>
2018-05-31 23:52:51 +0700

    modified current_user_full_name

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -20,6 +20,12 @@ module UsersHelper
   end
 
   def current_user_full_name
-    current_user.first_name + " " + current_user.last_name
+  #   # if current_user.first_name = nil || current_user.last_name == nil
+  #   #   current_user.name
+  #   # else
+  #   split_name(current_user.name)
+      current_user.first_name + " " + current_user.last_name
+      # user.first_name + " " + user.last_name
+  #   # end
   end
 end
