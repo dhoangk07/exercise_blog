a6fda25b0caab1297cea4f56548bc125c50fe4aa
HoangDinh <dhoangk07@gmail.com>
2018-06-03 14:18:44 +0700

    modified react helper

--- app/helpers/reacts_helper.rb
+++ app/helpers/reacts_helper.rb
@@ -2,4 +2,12 @@ module ReactsHelper
   def reacted?(user, newspaper)
     React.where(user_id: user.id, newspaper_id: newspaper.id).present?
   end
+
+  def reacted_by(user, newspaper, reaction)
+    React.create(user_id: user.id, newspaper_id: newspaper.id, reaction: reaction)
+  end
+
+  def unreacted_by(user, newspaper)
+    React.where(user_id: user.id, newspaper_id: newspaper.id).destroy_all
+  end
 end
